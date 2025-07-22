b8b4cca267bd44f5d9a0153423f488ee0651f039
Cesar Medina <cesarmd3742y@gmail.com>
2025-07-21 19:14:42 -0600

    no se que poner xd

---
+++ app.js
@@ -0,0 +1,27 @@
+//importacion de librerias 
+import mongoose from "mongoose"; // nos permite conectarnos a mongo
+import express from "express"; // crea el servidor
+import dotenv from "dotenv"; // trae variables de entorno del archivo .env
+import cors from "cors"; // seguridad
+import { error } from "console";
+
+
+dotenv.config(); // Acceder a la configuracion de dotenv
+
+// conectar una base de datos
+mongoose.connect(process.env.urlbd)
+.then(()=>{
+    console.log("conecto la base de datos")
+})
+.catch((error)=>{
+    console.log("No jalo la base ptm", error)
+})//promesa
+
+const app = express();//creando un servidor local
+app.use(cors()); //Seguridad en el servidor
+app.listen(4000, ()=> {
+    console.log("Se escucha el servidor")
+})// verificando funcionalidad del servidor
+
+
+
